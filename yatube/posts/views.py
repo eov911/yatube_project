0420d60292148ae54ade75898d7c8ab6852dc833
Оксана Ерегина <oksanaeregina@gmail.com>
2022-09-17 21:11:38 +0300

    Подключеная папка static. Bootstrap, logo заработали

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,7 +4,18 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    text = 'Это главная страница проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
 
 def group_posts(request, slug):
-    return HttpResponse(f'Страница с информацией о группе {slug}')
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text
+    }
+    return render(request, template, context)
+    
+
