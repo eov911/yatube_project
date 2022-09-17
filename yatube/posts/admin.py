cb6d58c8e56ac59e4fdd5fe60e2463a6e6cf6a28
Оксана Ерегина <oksanaeregina@gmail.com>
2022-09-17 22:44:30 +0300

    Создан класс PostAdmin в admin.py

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk','text','pub_date', 'author')
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+admin.site.register(Post, PostAdmin)
